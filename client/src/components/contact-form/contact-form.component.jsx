3273ce504a6d31572da7f162b5187e3671e29f6f
0mni-gear515 <adamyi515@gmail.com>
2022-03-03 18:12:32 -0600

    Stylize contact-form component

--- client/src/components/contact-form/contact-form.component.jsx
+++ client/src/components/contact-form/contact-form.component.jsx
@@ -1,9 +1,39 @@
 import React from 'react'
+import './contact-form.styles.css';
 
 const ContactForm = () => {
+  
+
+
+
+
+
+
   return (
-    <div>
-      
+    <div className='contact-form'>
+      <h2 className='text-center'>Add Contact</h2>
+      <form>
+        <div className='form-group'>
+          <input type='text' className='form-control' placeholder='Name' name='name' />
+        </div>
+        <div className='form-group'>
+          <input type='text' className='form-control' placeholder='Email' name='email' />
+        </div>
+        <div className='form-group'>
+          <input type='text' className='form-control' placeholder='phone' name='phone' />
+        </div>
+        <div className='form-group'>
+          <h4>Contact type:</h4>
+          <input type='radio' name='type' value='personal'/> Personal {' '}
+          <input type='radio' name='type' value='professional'/> Professional {' '}
+        </div>
+        <div className='form-group'>
+          <button className='btn btn-submit'>
+            Add Contact
+          </button>   
+        </div>
+            
+      </form>
     </div>
   )
 }
